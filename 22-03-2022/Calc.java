1696c060aa50fd479e92fe85269c15a5c3ab1979
shefanyshanavas <98250866+shefanyshanavas@users.noreply.github.com>
2022-03-22 12:58:04 +0530

    first commit

---
+++ 22-03-2022/Calc.java
@@ -0,0 +1,12 @@
+public class Calc{
+	public static void main(String args[]){
+		int a=10,b=20;
+		System.out.println("a="+a+"b="+b);
+		System.out.println("a + b ="+(a+b));
+		System.out.println("a - b ="+(a-b));
+		System.out.println("a * b ="+(a*b));
+		System.out.println("a / b ="+(a/b));
+		System.out.println("a % b ="+(a%b));
+	}
+}
+	  
---
+++ 22-03-2022/Sample.java
@@ -0,0 +1,8 @@
+public class Sample{
+	public static void main(String args[]){
+		String str1="HELLOW";
+		String str2="JAVA";
+		String str3=str1+str2;
+		System.out.println(str3);
+	}
+}
